1c4647ef7ebb01cb408786cd3997295d09f2ce43
Silvia Chen <welllife1010@gmail.com>
2019-11-16 19:50:45 -0800

    updated Search.js in src / js / models

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,6 +1,7 @@
 // https://www.food2fork.com/api/search
 // 1e5f7a74e8e75669e9909210e1830349
 import axios from 'axios'; // axios automatically returns json
+import { key, proxy } from '../config';
 
 export default class Search {
     constructor(query) {
@@ -8,11 +9,9 @@ export default class Search {
     }
 
     async getResults() {
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key = '1e5f7a74e8e75669e9909210e1830349';
         try {
-            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`); // returns an Object
-            //const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+            //const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`); // returns an Object
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
             console.log(this.result); // an array of objects
         } catch(error) {
