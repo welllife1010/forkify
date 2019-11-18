d6ee0da14d7c0b11fc328debac88cc7e559b3161
Silvia Chen <welllife1010@gmail.com>
2019-11-17 19:11:47 -0800

    updated index.js
    added recipeView functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -69,6 +70,11 @@ const controlRecipe = async () => {
 
     if(id) {
         // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe); // pass in the parent
+
+        // Hightlight selected search item
+        if (state.search) searchView.highlightSelected(id); // if there was a search
 
         // Create new recipe object
         state.recipe = new Recipe(id);
@@ -83,7 +89,8 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             // Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (err) {
             console.log(err);
             alert('Error processing recipe!');
@@ -93,3 +100,20 @@ const controlRecipe = async () => {
 };
 
 ['load', 'hashchange'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // Decrease button is clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // Increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+    console.log(state.recipe);
+});
